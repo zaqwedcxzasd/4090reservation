c5db3e0309960c9810284e06ecc26c2c8779b610
zaqwedcxzasd <zaqwedcxzasd@gmail.com>
2025-12-01 04:40:03 +0900

    Update auto_reserve.py

--- auto_reserve.py
+++ auto_reserve.py
@@ -17,7 +17,7 @@ def main():
         purpose="定期ジョブ",
         start_dt=start_dt,
         end_dt=end_dt,
-        memory_gb=0.0,
+        memory_gb=1.0,
     )
 
     print(f"[auto_reserve] {start_dt} - {end_dt} を予約しました。")
